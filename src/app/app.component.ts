9dfdc2da8587bf46be2bfa3d4508af9b2e1fe305
Greta Sund <greta.sund@gmail.com>
2024-05-09 18:56:47 +0800

    Routing to register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SplashScreenComponent } from "./splash-screen/splash-screen.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SplashScreenComponent],
+  imports: [RouterOutlet, RouterLink, SplashScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
