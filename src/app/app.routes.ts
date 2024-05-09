fab325ad55b712b0c083f5eaa553c157e580df8c
Kaja <111892274+kaja1998@users.noreply.github.com>
2024-05-09 15:03:27 +0200

    Register Seite finished. Links fehlen noch Splash Screen noch angepasst damit die Abstände für jedes Smartphone angepasst werden.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import {RegisterComponent} from "./register/register.component";
 import {SplashScreenComponent} from "./splash-screen/splash-screen.component";
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: SplashScreenComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: AppComponent }
+  { path: 'home', component: AppComponent },
+  { path: 'login', component: LoginComponent }
 ];
