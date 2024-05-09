1099b8ae17d38f81bb09d690afe1fa8ddda5767b
Greta Sund <greta.sund@gmail.com>
2024-05-09 18:41:36 +0800

    Reorg components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import {MatButton} from "@angular/material/button";
+import { SplashScreenComponent } from "./splash-screen/splash-screen.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatButton],
+  imports: [RouterOutlet, SplashScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-splash-screen',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton],
   templateUrl: './splash-screen.component.html',
   styleUrl: './splash-screen.component.css'
 })
