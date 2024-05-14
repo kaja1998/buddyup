1f916b4bea532c9aac2c9bc54e6ec2514665537c
Kaja <111892274+kaja1998@users.noreply.github.com>
2024-05-14 14:24:07 +0200

    Form Validation register-form-email.component

--- src/app/register-form-email/register-form-email.component.ts
+++ src/app/register-form-email/register-form-email.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register-form-email',
+  selector: 'app-register-form-email.component',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './register-form-email.component.html',
   styleUrl: './register-form-email.component.css'
 })
-export class RegisterFormEmailComponent {
+export class RegisterFormEmailComponent implements OnInit {
+
+  userForm!: FormGroup;
+  isFormSubmitted: boolean = false;
+
+  ngOnInit() {
+    this.userForm =  new FormGroup({
+      firstName: new FormControl("",[Validators.required]),
+      lastName: new FormControl("",[Validators.required,Validators.minLength(4)]),
+      userName:  new FormControl("",[Validators.required,Validators.email]),
+      city: new FormControl(""),
+      state: new FormControl(""),
+      zipcode: new FormControl(""),
+      isAgree: new FormControl(false)
+    })
+  }
+
+  onSubmit() {
+    const isFormValid = this.userForm.valid;
+    debugger;
+    this.isFormSubmitted =  true;
+  }
 
 }
