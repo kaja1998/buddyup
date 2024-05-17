9d83fea4ee795ca0e0cd3283a6a11a647e516777
Greta Sund <greta.sund@gmail.com>
2024-05-17 22:01:17 +0800

    State select in angular material

--- src/app/register-form-email/register-form-email.component.ts
+++ src/app/register-form-email/register-form-email.component.ts
@@ -1,15 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField } from "@angular/material/form-field";
+import { MatOption, MatSelect, MatLabel } from "@angular/material/select";
 
 @Component({
   selector: 'app-register-form-email.component',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, MatFormField, MatSelect, MatOption, MatLabel],
   templateUrl: './register-form-email.component.html',
   styleUrl: './register-form-email.component.css'
 })
 export class RegisterFormEmailComponent implements OnInit {
+
+  selectedState: string = "";
+  states: String[] = [
+    "Bayern",
+    "Baden-Württemberg",
+    "Berlin",
+    "Brandenburg",
+    "Bremen",
+    "Hamburg",
+    "Hessen",
+    "Mecklenburg-Vorpommern",
+    "Niedersachsen",
+    "Nordrhein-Westfalen",
+    "Saarland",
+    "Sachsen-Anhalt",
+    "Sachsen",
+    "Schleswig-Holstein",
+    "Thüringen",
+    "Rheinland-Pfalz"
+  ];
 
   userForm!: FormGroup;
   //protected readonly FormGroup = FormGroup;
