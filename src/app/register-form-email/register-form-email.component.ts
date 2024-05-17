f1f270a2291de4139c408dc4ea5f88e2adb32170
Greta Sund <greta.sund@gmail.com>
2024-05-17 23:34:16 +0800

    Add comments to RegisterFormEmail html, add password input field (Angular Material)

--- src/app/register-form-email/register-form-email.component.ts
+++ src/app/register-form-email/register-form-email.component.ts
@@ -3,17 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormField } from "@angular/material/form-field";
 import { MatOption, MatSelect, MatLabel } from "@angular/material/select";
+import {MatInput} from "@angular/material/input";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-register-form-email.component',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, MatFormField, MatSelect, MatOption, MatLabel],
+  imports: [ReactiveFormsModule, CommonModule, MatFormField, MatSelect, MatOption, MatLabel, MatInput, MatIconButton, MatIcon],
   templateUrl: './register-form-email.component.html',
   styleUrl: './register-form-email.component.css'
 })
 export class RegisterFormEmailComponent implements OnInit {
 
-  selectedState: string = "";
   states: String[] = [
     "Bayern",
     "Baden-Württemberg",
@@ -32,6 +34,7 @@ export class RegisterFormEmailComponent implements OnInit {
     "Thüringen",
     "Rheinland-Pfalz"
   ];
+  hide = true;
 
   userForm!: FormGroup;
   //protected readonly FormGroup = FormGroup;
@@ -41,11 +44,12 @@ export class RegisterFormEmailComponent implements OnInit {
     this.userForm =  new FormGroup({
       firstName: new FormControl("",[Validators.required]),
       lastName: new FormControl("",[Validators.required, Validators.minLength(2)]),
+      birthDate: new FormControl("",[Validators.required]),
       userName:  new FormControl("",[Validators.required, Validators.minLength(2)]),
       email:  new FormControl("",[Validators.required, Validators.email, Validators.minLength(2)]),
       city: new FormControl("",[Validators.required, Validators.minLength(2)]),
       state: new FormControl("", [Validators.required]),
-      zipcode: new FormControl("", [Validators.required]),
+      zipcode: new FormControl("", [Validators.required, Validators.minLength(4)]),
       isAgree: new FormControl(false, [Validators.required])
     })
   }
